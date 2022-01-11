a213c49bbbee5f98be10f6d7e348ed46ea4ee3ab
SeongGwangKim <78336335+SeongGwangKim@users.noreply.github.com>
2022-01-11 19:48:27 +0900

    실수로 삭제하여 백업파일로 업로드

---
+++ flaskRestAPI/img_download.py
@@ -0,0 +1,25 @@
+import cv2
+import requests
+from io import BytesIO
+from PIL import Image
+import numpy as np
+import urllib.request
+
+def download_img(filename):
+
+    # 이미지 다운로드
+    urllib.request.urlretrieve(
+        "https://transimagetest.s3.ap-northeast-2.amazonaws.com/"+filename,
+        filename)
+
+    # 다운받을 이미지 url
+    url = "https://transimagetest.s3.ap-northeast-2.amazonaws.com/"+filename
+    res = requests.get(url)
+
+    #Img open
+    img = np.asarray(Image.open(BytesIO(res.content)))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
+
+if __name__ == "__main__":
+    download_img("temp.jpg")
